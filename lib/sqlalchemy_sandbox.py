1d4540da7050a4a2a0593f22d799ced41c50dfe1
eddy <eddy.mwaniki.27@gmail.com>
2025-03-04 16:39:09 +0300

    creates student sql table

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Student(Base):
-    pass
+class Student(Base) :
+    __tablename__ = "students"
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///students.db")
+    Base.metadata.create_all(engine) 
+
+    
